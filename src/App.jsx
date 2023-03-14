8c447eff96d44806a8a4c6c05d1f7ddb664c921e
danpeaku <daniel.sandoval@peaku.ai>
2023-03-14 14:55:06 -0500

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Navbar from './components/Navbar'
+import GamesBar from './components/Games-list'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+      <Navbar />
+      <GamesBar />
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+
+
+const Navbar = () => {
+  
+
+  return (
+    <section>
+      <div className="navbar-container">
+        <div className="logo">
+          <img src="images/logo.png" alt="" />
+        </div>
+        <div className="nav-links">
+          <ul className="nav-ul">
+            <li className="nav-item">
+              <a href="" className="nav-link">Nosotros</a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">Programacion</a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">Eventos</a>
+            </li>
+            <li className="nav-item">
+              <a href="" className="nav-link">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Navbar;
