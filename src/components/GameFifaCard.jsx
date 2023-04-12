9a3aef45c145f435e64e9f346ad8e6d2ec69b569
dan33311 <dansaro.groger@gmail.com>
2023-04-12 12:16:48 -0500

    apply minimal style changes to the Game component

--- src/components/GameFifaCard.jsx
+++ src/components/GameFifaCard.jsx
@@ -8,23 +8,23 @@ const GameFifaCard = () => {
         <div className="game-tags">
           <div className="tag-container">
             <img src="/images/polygon-tag.png" alt="" />
-            <p>Sports</p>
+            <p className="card-variant-color">Sports</p>
           </div>
           <div className="tag-container">
             <img src="/images/polygon-tag.png" alt="" />
-            <p>1 vs 1</p>
+            <p className="card-variant-color">1 vs 1</p>
           </div>
           <div className="tag-container">
             <img src="/images/polygon-tag.png" alt="" />
-            <p>PC</p>
+            <p className="card-variant-color">PC</p>
           </div>
         </div>
 
         <p className="game-desc">Podrán participar en la actividad las personas: (I) naturales mayores de dieciocho (18) años, (II) residentes en Bogotá y Cundinamarca, (III) que estén registrados en el enlace descrito en la mecánica.</p>
 
-        <h4>Formato: </h4>
+        <h4 className="card-variant-color">Formato: </h4>
         <p>Eliminacion sencilla.</p>
-        <h4>premiacion: </h4>
+        <h4 className="card-variant-color">premiacion: </h4>
         <p>Se entregarán premios al primer, segundo, y tercer lugar del evento.</p>
       </div>
 
